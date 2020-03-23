3a9a3f4704d824a19b61f357af3619c67701f115
Nathan Reitan <nathanreitan@gmail.com>
2020-03-23 11:21:30 -0700

    complete

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var header = document.getElementById("header");
 var pageHeader= new PageHeader(header)
 var table = document.getElementById("grade-table");
 var gradeTable = new GradeTable(table);
-
-var getGrades = new App(gradeTable, pageHeader);
+var formArr = document.querySelector("form");
+var gradeForm = new GradeForm(formArr);
+var getGrades = new App(gradeTable, pageHeader, gradeForm);
 getGrades.start();
